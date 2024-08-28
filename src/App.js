5b4e0495fbfc6d3991ebdc2873fa18eee17d39ec
anttilan <antti.lankinen@helsinki.fi>
2024-08-28 09:10:24 +0300

    Header työn alla.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,60 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translations from './translations';
 import './App.css';
 import CourseList from "./components/CourseList";
 import {Provider} from "react-redux";
 import {applyMiddleware, createStore} from "redux";
 import {thunk} from "redux-thunk";
 import courseReducer from './reducers';
+import {Col, Container, Row} from "react-bootstrap";
+import Header from "./components/header/Header";
+import {DEFAULT_LANGUAGE} from "./Constants";
 
 const store = createStore(courseReducer, applyMiddleware(thunk));
+
+const defaultLanguage = () => {
+    try {
+        const savedLanguage = localStorage.getItem('language');
+        if (savedLanguage) {
+            document.documentElement.lang = savedLanguage;
+            return savedLanguage;
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+    document.documentElement.lang = DEFAULT_LANGUAGE;
+    return DEFAULT_LANGUAGE;
+};
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: translations,
+        lng: defaultLanguage(),
+        fallbackLng: 'cimode',
+        supportedLngs: ['fi', 'en', 'sv']
+    });
+
+//listenForBreakpointChanges();
+
 
 function App() {
   return (
         <Provider store={store}>
-            <CourseList />
+            <Container className="root mx-0">
+                <Row className="header-row mb-2">
+                    <Col as="header" role="banner" className="px-0">
+                        <Header />
+                    </Col>
+                </Row>
+                <Row className="root-main-row">
+                    <CourseList />
+                </Row>
+            </Container>
         </Provider>
   );
 }
---
+++ src/components/header/Logo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+//import useSearchParams from '../../hooks/useSearchParams';
+import HyColors from '../utilities/HyColors';
+import HyLogo from '../utilities/HyLogo';
+//import { belowBreakpoint, leftSideIsHidden, hideLeft } from '../utilities/visibilities';
+import './Logo.css';
+
+const Logo = () => {
+    const { t } = useTranslation();
+    //const [_searchParams, setSearchParams] = useSearchParams();
+
+    const goToFrontPage = () => {
+        //setSearchParams({});
+        //if (belowBreakpoint()) {
+        //    hideLeft();
+        //}
+    };
+
+    const onClick = (event) => {
+        event.preventDefault();
+        goToFrontPage();
+    };
+
+    return (
+        <div className="logo">
+            <HyLogo width={48} height={48} fill={HyColors.black} />
+            <a href="/" onClick={onClick}>
+                <h1>
+                    {t('Reflection Compass')}
+                </h1>
+            </a>
+        </div>
+    );
+};
+
+Logo.propTypes = {
+};
+
+export default Logo;
