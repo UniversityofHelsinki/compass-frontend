e252bbf76c5905ae3680964785e3eb9f6e08ede3
Vellu <veli-matti.heikkila@helsinki.fi>
2024-08-30 08:40:58 +0300

    change the login url use test setup in build stage

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 const COMPASS_BACKEND_SERVER = process.env.REACT_APP_COMPASS_BACKEND_SERVER || '';
 
-const login = (url = process.env.REACT_APP_LATAAMO_LOGIN) => {
+const login = (url = process.env.REACT_APP_COMPASS_LOGIN) => {
     window.location.replace(url);
 };
 
