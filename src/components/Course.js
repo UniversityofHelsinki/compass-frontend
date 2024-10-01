3685c9c570a92e4da76211e9792c710f9cda179d
anttilan <antti.lankinen@helsinki.fi>
2024-10-01 13:22:26 +0300

    Kurssilista.

---
+++ src/components/Course.js
@@ -0,0 +1,23 @@
+import {Component} from "react";
+import * as PropTypes from "prop-types";
+import './Course.css'
+
+const Course = ({course}) => {
+        return(
+            <div className="student-course">
+                    <div className="student-course-title">
+                        <span>
+                            {course.title}
+                        </span>
+                    </div>
+                    <div className="student-course-description">
+                        <span>
+                            {course.description}
+                        </span>
+                    </div>
+                </div>
+        )
+}
+
+Course.propTypes = {course: PropTypes.any};
+export default Course;
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,34 +1,45 @@
 import useAllCourses from "../hooks/useAllCourses";
 import './CourseList.css'
 import DropDown from "../form/DropDown";
-import {useState} from "react";
+import {Component, useState} from "react";
 import {useTranslation} from "react-i18next";
+import * as PropTypes from "prop-types";
+import Course from "./Course";
+
 const CourseList = () => {
     const { t } = useTranslation();
-
     const [courses,  _loading, _reload] = useAllCourses(
         {load: true}
     )
 
     const [selectedOption, setSelectedOption] = useState("");
 
-    const handleChange = (option) => {
-        setSelectedOption(option);
-    };
+    const listCourses = () => {
+        return(
+            <div>
+                <ul className="course-list">
+                    {courses && courses?.length > 0 && courses?.map((course) =>
+                      <li className="course-row">
+                          <Course  course={course}/>
+                      </li>
+                    )}
+                </ul>
+            </div>)
+    }
 
-    return (
-        <>
-            <label className="label" id="dropdown"> {t('courses')}</label>
-            <DropDown
-                onChange={(e) => handleChange(e.target.value)}
-                selectedOption={selectedOption}
-                options={courses}
-                id="dropdown"
-            />
-        </>
-    )
+    const noCoures = () => {
+        return(
+            <div className="no-courses"> {t('student_no_courses')}
+            </div>
+        )
+    }
 
+    if (courses) {
+        return listCourses();
+    }
+    return noCoures();
 }
 
+CourseList.propTypes = {};
 
 export default CourseList;
