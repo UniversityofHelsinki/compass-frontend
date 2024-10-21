eea9229e2a49816e5a15dd9ee313238839825e04
anttilan <antti.lankinen@helsinki.fi>
2024-10-21 07:39:05 +0300

    korjaus

--- src/components/delete/DeleteStudentAnswerFooter.js
+++ src/components/delete/DeleteStudentAnswerFooter.js
@@ -6,14 +6,15 @@ import HyButton from '../utilities/HyButton';
 import {Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
-const DeleteStudentAnswerFooter = ({disabled, assignment, deleteAnswer}) => {
+const DeleteStudentAnswerFooter = ({disabled, assignment, course, deleteAnswer}) => {
     const { t } = useTranslation();
+    let answer = assignment;
 
     return (
         <Container>
             <div className="delete-student-answer-footer">
                 <div className="delete-student-answer-footer-buttons">
-                    <Link to={`/student/feedback/${assignment}`} >
+                    <Link to={`/student/feedback/${answer}/${course}`} >
                         <HyButton variant="primary" onClick={null} type="submit" disabled={disabled}>
                             {t('assignment_answer_do_not_remove')}
                         </HyButton>
