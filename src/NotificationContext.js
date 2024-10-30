db70955646ee6d136890b1b22951b68ca4e18ef0
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-10-30 08:52:43 +0200

    SC-108 -- teacher can copy links & notification provider

---
+++ src/NotificationContext.js
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const NotificationContext = createContext(null);
+
+export const useNotification = () => {
+    return useContext(NotificationContext);
+};
+
+const NotificationProvider = ({ children }) => {
+    const [notification, setNotification] = useState(null);
+
+    const clearNotification = () => {
+        setNotification(null);
+    };
+
+    const addNotification = (
+        content,
+        type = 'neutral',
+        disappearing = false,
+        additionalContent = '',
+    ) => {
+        const allowedTypes = ['error', 'success', 'neutral'];
+
+        if (!allowedTypes.includes(type)) {
+            throw new Error(`Invalid type ${type}. Accepted are ${allowedTypes}`);
+        } else if (!content) {
+            throw new Error(`Notification must have content defined (${content}).`);
+        }
+
+        setNotification({ content, type, disappearing, additionalContent });
+    };
+
+    return (
+        <NotificationContext.Provider
+            value={{
+                notification,
+                setNotification: addNotification,
+                clearNotification,
+            }}
+        >
+            {children}
+        </NotificationContext.Provider>
+    );
+};
+
+export default NotificationProvider;
