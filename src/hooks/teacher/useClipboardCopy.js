d975302ca1002760b03049cfdd89683778569e9a
Vellu <veli-matti.heikkila@helsinki.fi>
2024-11-18 10:17:02 +0200

    get signature for url and use it

--- src/hooks/teacher/useClipboardCopy.js
+++ src/hooks/teacher/useClipboardCopy.js
@@ -11,9 +11,6 @@ const useClipboardCopy = (id, courseId) => {
     const [signature, setSignature] = useState(null);
     const [shouldFetch, setShouldFetch] = useState(false);
 
-    console.log(`/api/getUrlSignature/${courseId}`);
-
-    // Use your custom useGET hook to fetch the signature
     const [value, fetchError, reload] = useGET({
         path: `/api/getUrlSignature/${courseId}`,
         tag: `signature-${courseId}`,
@@ -24,12 +21,11 @@ const useClipboardCopy = (id, courseId) => {
             reload();
             setShouldFetch(false);
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [shouldFetch]);
 
     useEffect(() => {
         if (value) {
-            setSignature(value); // Assuming backend returns signature as plain string
+            setSignature(value);
         }
         if (fetchError) {
             setError(fetchError);
@@ -37,23 +33,20 @@ const useClipboardCopy = (id, courseId) => {
     }, [value, fetchError]);
 
     const clipboardCopy = async () => {
-        setError(null); // Clear previous error messages
+        setError(null);
         setLoading(true);
-        setShouldFetch(true); // Trigger data fetching
+        setShouldFetch(true);
 
         if (signature) {
             try {
                 const target = `${window.location.origin}/student/assignments/${id}?signature=${signature}`;
-                // Copy the URL with the signature to the clipboard
                 await navigator.clipboard.writeText(target);
                 setNotification(t('teacher_forms_table_share_copy_to_clipboard'), 'success', true);
             } catch (error) {
-                console.error('Error copying to clipboard:', error);
                 setError(error.message);
                 setNotification(t('error_copying_to_clipboard'), 'error', true);
             }
         } else {
-            setError('Signature not available');
             setNotification(t('error_generating_signed_url'), 'error', true);
         }
         setLoading(false);
