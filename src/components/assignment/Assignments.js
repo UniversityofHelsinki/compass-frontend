07ea32db919b2b68b64d01b35f79c7833c4a857b
anttilan <antti.lankinen@helsinki.fi>
2024-11-18 14:06:40 +0200

    Opiskelijanäkymän muutokset.

--- src/components/assignment/Assignments.js
+++ src/components/assignment/Assignments.js
@@ -1,27 +1,38 @@
 import './Assignments.css';
-import Row from 'react-bootstrap/Row';
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import useStudentCourseAssignmentAnswer from '../../hooks/useStudentCourseAssignmentAnswer';
 import useStudentCourse from '../../hooks/useStudentCourse';
 import TopBar from '../utilities/TopBar';
-//import useStudentAnswer from "../../hooks/useStudentAnswer";
+import PropTypes from 'prop-types';
 
 const AssignmentListItem = ({ assignment, href }) => {
     const { t } = useTranslation();
+    let anwer =
+        assignment?.answered === true ? t('assignments_answered') : t('assignments_not_answered');
     return (
         <div className="assignments-list-item">
             <div className="assignments-list-item-link">
-                <Link to={href}>{assignment?.topic}</Link>
-                <span className="assignments-list-item-answered">{assignment.answered}</span>
+                <Link to={href} className="assignments-list-item-link">
+                    {' '}
+                    {assignment?.topic}{' '}
+                </Link>
+                <span
+                    className={
+                        assignment?.answered === true
+                            ? 'assignments-list-item-answered'
+                            : 'assignments-list-item-not-answered'
+                    }
+                >
+                    {anwer}
+                </span>
             </div>
             <div className="assignments-list-item-secondary">
                 <span className="screenreader-only">{t('assignment_list_item_period')}</span>
                 <span>
-                    {new Date(assignment.start_date).toLocaleDateString('fi-FI')} -{' '}
-                    {new Date(assignment.end_date).toLocaleDateString('fi-FI')}
+                    {new Date(assignment?.start_date).toLocaleDateString('fi-FI')} -{' '}
+                    {new Date(assignment?.end_date).toLocaleDateString('fi-FI')}
                 </span>
             </div>
         </div>
@@ -58,9 +69,7 @@ const Assignments = () => {
                 showBackBtn={true}
                 backBtnHref={backBtnHref}
                 backBtnLabels={backBtnLabels}
-            >
-
-            </TopBar>
+            ></TopBar>
             <div className="m-3"></div>
             <div className="responsive-margins">
                 <h3>{t('assignments_due')}</h3>
@@ -70,7 +79,11 @@ const Assignments = () => {
                         <li key={assignment.id}>
                             <AssignmentListItem
                                 assignment={assignment}
-                                href={`/student/assignment/${assignment?.id}/${course?.id}`}
+                                href={
+                                    assignment.answered === true
+                                        ? `/student/feedback/${assignment?.id}/${course?.course_id}/${course?.id}`
+                                        : `/student/assignment/${assignment?.id}/${course?.id}`
+                                }
                             />
                         </li>
                     ))}
@@ -89,16 +102,22 @@ const Assignments = () => {
                     ))}
                 </ul>
 
-                <div className="assignments-reflection-summary">
-                    <Link to={`/student/courses/${course?.course_id}/summary`}>
-                        {t('assignments_summary')}
-                    </Link>
+                <h3>{t('assignments_reflection_summary')}</h3>
+                <div className="assignments-list-item">
+                    <div className="assignments-list-item-link">
+                        <Link to={`/student/courses/${course?.course_id}/summary`}>
+                            {t('assignments_summary')}
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
     );
 };
 
-Assignments.propTypes = {};
+Assignments.propTypes = {
+    assignment: PropTypes.object,
+    href: PropTypes.string,
+};
 
 export default Assignments;
