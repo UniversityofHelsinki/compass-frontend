3819fe6dd39a7d4271fb7a6e8d8b42fedb404e01
Vellu <veli-matti.heikkila@helsinki.fi>
2024-11-28 16:40:27 +0200

    removed console.logs

--- src/hooks/useStudentCourseAssignmentAnswer.js
+++ src/hooks/useStudentCourseAssignmentAnswer.js
@@ -20,7 +20,6 @@ const useStudentCourseAssignmentAnswer = (course_id, signature, id) => {
     //const [previousAssignments, setPreviousAssignments] = useState(null);
     const get = async (course_id) => {
         if (course_id) {
-            console.log(signature);
             const COMPASS_BACKEND_SERVER = process.env.REACT_APP_COMPASS_BACKEND_SERVER || '';
             const URL = `${COMPASS_BACKEND_SERVER}/api/student/course/assignment/answer/${course_id}?id=${id}&signature=${signature}`;
             try {
@@ -59,12 +58,7 @@ const useStudentCourseAssignmentAnswer = (course_id, signature, id) => {
                 }
             });
 
-    if (!(assignments[0]?.course_state === COURSE_ONGOING) && errMsg === null) {
-        console.log(
-            'COURSE_NOT_ONGOING',
-            assignments[0]?.course_state,
-            assignments[0]?.course_date,
-        );
+    if (assignments[0]?.course_state !== COURSE_ONGOING && errMsg === null) {
         setErrMsg(assignments[0]?.course_state);
         setCourseDate(assignments[0]?.course_date);
     }
