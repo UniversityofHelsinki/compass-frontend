dceb1ab844437e9261b30e9473eb9cac963c40da
jooaho <jooaho@helsinki.fi>
2024-12-02 14:23:09 +0200

    Small a11y updates, delete answer button fix

--- src/components/delete/DeleteStudentAnswerFooter.js
+++ src/components/delete/DeleteStudentAnswerFooter.js
@@ -3,23 +3,28 @@ import PropTypes from 'prop-types';
 import './DeleteStudentAnswerFooter.css';
 import { useTranslation } from 'react-i18next';
 import HyButton from '../utilities/HyButton';
-import {Container} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const DeleteStudentAnswerFooter = ({disabled, answer, course, id, deleteAnswer}) => {
+const DeleteStudentAnswerFooter = ({ disabled, answer, course, id, deleteAnswer }) => {
     const { t } = useTranslation();
 
     return (
         <Container>
             <div className="delete-student-answer-footer">
                 <div className="delete-student-answer-footer-buttons">
-                    <Link to={`/student/feedback/${answer}/${course}/${id}`} >
-                        <HyButton variant="primary" onClick={null} type="submit" disabled={disabled}>
+                    <Link to={`/student/feedback/${answer}/${course}/${id}`}>
+                        <HyButton variant="primary" type="submit" disabled={disabled}>
                             {t('assignment_answer_do_not_remove')}
                         </HyButton>
                     </Link>
-                    <Link to={`/student/assignments/${answer}/${course}/${id}`} >
-                        <HyButton variant="primary" type="submit" onClick={deleteAnswer} disabled={disabled}>
+                    <Link to={`/student/assignments/${answer}/${course}/${id}`}>
+                        <HyButton
+                            variant="danger"
+                            type="submit"
+                            onClick={deleteAnswer}
+                            disabled={disabled}
+                        >
                             {t('assignment_answer_remove')}
                         </HyButton>
                     </Link>
@@ -34,7 +39,7 @@ DeleteStudentAnswerFooter.propTypes = {
     answer: PropTypes.number,
     course: PropTypes.string,
     id: PropTypes.number,
-    deleteAnswer: PropTypes.func
+    deleteAnswer: PropTypes.func,
 };
 
 export default DeleteStudentAnswerFooter;
