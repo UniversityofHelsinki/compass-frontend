5a6fc8faa1f6911775caa74a75a13df6348e10ab
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-12-04 07:19:22 +0200

    Merge import

--- src/components/utilities/TopBar.js
+++ src/components/utilities/TopBar.js
@@ -1,36 +1,27 @@
 import './TopBar.css';
-import BackButton from './BackButton';
+import BackButton, { propTypes as BackButtonPropTypes } from './BackButton';
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { propTypes as BackButtonPropTypes } from './BackButton';
 
-const TopBar = ({ 
-  showBackBtn = false,
-  backBtnLabels,
-  backBtnHref = '',
-  heading,
-  children
-}) => {
-  return (
-    <div className="top-bar">
-      <div className="top-bar-back-btn">
-        {showBackBtn && <BackButton labels={backBtnLabels} href={backBtnHref} />}
-      </div>
-      <div className="top-bar-heading">
-        <h2>{heading}</h2>
-      </div>
-      <div className="top-bar-children">
-        {children}
-      </div>
-    </div>
-  );
+const TopBar = ({ showBackBtn = false, backBtnLabels, backBtnHref = '', heading, children }) => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-back-btn">
+                {showBackBtn && <BackButton labels={backBtnLabels} href={backBtnHref} />}
+            </div>
+            <div className="top-bar-heading">
+                <h2>{heading}</h2>
+            </div>
+            <div className="top-bar-children">{children}</div>
+        </div>
+    );
 };
 
 export const propTypes = {
-  children: PropTypes.node,
-  showBackBtn: PropTypes.bool,
-  backBtnLabels: BackButtonPropTypes.labels,
-  backButtonHref: BackButtonPropTypes.href
+    children: PropTypes.node,
+    showBackBtn: PropTypes.bool,
+    backBtnLabels: BackButtonPropTypes.labels,
+    backButtonHref: BackButtonPropTypes.href,
 };
 
 TopBar.propTypes = propTypes;
