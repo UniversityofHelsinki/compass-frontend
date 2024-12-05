624ac302f44884a4266106c3faddb548a2a0260d
anttilan <antti.lankinen@helsinki.fi>
2024-12-05 11:58:40 +0200

    Testiä päivitetty

--- src/components/assignment/Assignment.test.js
+++ src/components/assignment/Assignment.test.js
@@ -3,7 +3,24 @@ import { render, screen } from '../../test/render.js';
 import Assignment from './Assignment';
 
 it('renders', () => {
-    render(
-        <Assignment />
-    );
+    render(<Assignment levels={[]} />);
+});
+
+describe('renders', () => {
+    let component;
+    beforeEach(() => {
+        component = render(<Assignment levels={[]} />);
+    });
+
+    test('top-bar-heading', () => {
+        expect(screen.getByText('assignment_answer_back_to_course')).toBeInTheDocument();
+    });
+
+    test('form_clear', () => {
+        expect(screen.getByText('form_clear')).toBeInTheDocument();
+    });
+
+    test('form_submit', () => {
+        expect(screen.getByText('form_submit')).toBeInTheDocument();
+    });
 });
