4dd8239fcbbab28e2f51d1cce044c400d140e1d1
Riku Hänninen <riku.hanninen@helsinki.fi>
2025-01-14 11:26:37 +0200

    add test

--- src/components/teacher/TeacherForms.test.js
+++ src/components/teacher/TeacherForms.test.js
@@ -32,3 +32,25 @@ test('No courses disclaimer is shown', () => {
 
     expect(document.querySelector('.teacher-forms-no-forms')).toBeDefined();
 });
+
+test('Course table is shown when there are courses', () => {
+    render(
+        <TeacherForms />,
+        {},
+        {
+            teacher: {
+                courses: [
+                    {
+                        title: 'Research Ethics',
+                        course_id: 'REE',
+                        start_date: new Date().toISOString(),
+                        end_date: new Date().toISOString(),
+                    },
+                ],
+            },
+        },
+    );
+    expect(screen.queryByRole('table')).toBeVisible();
+    expect(screen.queryByText('Research Ethics')).toBeVisible();
+    expect(screen.queryByText('REE')).toBeVisible();
+});
